85ddedd9f59ae79dbd5d92048f4b437f011a772f
MichaelAbe <michael.k.abe@gmail.com>
2021-04-07 09:10:54 -0400

    setting up routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import Err from './components/Err'
+
+
+export class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={ Home} />
+          <Route component={ Err } />
+        </Switch>
+        <Footer />
+
+      </Router>
+     
+    )
+  
+  }
 }
 
-export default App;
+export default App
+
+
+
---
+++ src/components/Err.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Err extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Oh no! An error has occured!</h3>
+            </div>
+        )
+    }
+}
+
+export default Err
